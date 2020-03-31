01ada22828d4c8268b670a8612932a61f5051d89
scott <scott@scottjjohnson.com>
2020-03-31 15:13:18 -0400

    Update website url

--- scripts/MinorHighLowIndicatorSTUDY.ts
+++ scripts/MinorHighLowIndicatorSTUDY.ts
@@ -1,5 +1,5 @@
 #
-# Copyright 2019 Scott J. Johnson (http://scottjjohnson.com)
+# Copyright 2019 Scott J. Johnson (https://scottjjohnson.com)
 #
 # Licensed under the Apache License, Version 2.0 (the "License");
 # you may not use this file except in compliance with the License.
