db15b843db9e64870e0a72d1e0970800f35e05d5
Scott <scott@scottjjohnson.com>
2023-08-20 14:11:14 -0400

    change chart to histogram and add min buyable ATR plot line

--- scripts/ATRPercentSTUDY.ts
+++ scripts/ATRPercentSTUDY.ts
@@ -1,5 +1,5 @@
 #
-# Copyright 2014 Scott J. Johnson (https://scottjjohnson.com)
+# Copyright 2023 Scott J. Johnson (https://scottjjohnson.com)
 #
 # Licensed under the Apache License, Version 2.0 (the "License");
 # you may not use this file except in compliance with the License.
@@ -17,11 +17,16 @@
 #
 # ATRPercent
 #
-# Calculates the ATR as a percentage of the last closing price.
+# Calculates the ATR as a percentage of the closing price.
 #
 declare lower;
 
 input ATRLength = 40;
+input ATRMinBuyable = 1.90;  # minimum ATR for stock to be buyable
 
 plot ATR = Round(Average(TrueRange(high, close, low), ATRLength) / close * 100,2);
-ATR.SetDefaultColor(GetColor(8));  # yellow
+ATR.SetPaintingStrategy(PaintingStrategy.HISTOGRAM);
+ATR.AssignValueColor(if ATR >= ATRMinBuyable then Color.GREEN else Color.RED);
+
+plot minBuyableLine = ATRMinBuyable;
+minBuyableLine.SetDefaultColor(Color.WHITE); 
